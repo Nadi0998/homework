5708dabe2e318e59c2ad95c9b7dd3a3b5168817c
Oleg <rumyantsevok@gmail.com>
2019-01-08 19:43:01 +0300

    Would it work?

--- homework/urls.py
+++ homework/urls.py
@@ -19,18 +19,20 @@ from main_app.views import *
 from django_registration.backends.one_step.views import RegistrationView
 
 urlpatterns = [
-    # path(r'accounts/', include('django_registration.backends.one_step.urls')),
-    path('accounts/login', name='login'),
-    path('accounts/logout', name='logout'),
+    path(r'accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/register/', RegistrationView.as_view(form_class=CustomUserRegistrationForm), name='register_user'),
+
+    path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/profile/', ProfileView.as_view(), name='user_profile'),
     path('accounts/profile/update/', UpdateProfileView.as_view(), name='update_user_profile'),
-    path('accounts/register/', RegistrationView.as_view(form_class=CustomUserRegistrationForm), name='register'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
+
     path('flowers/', FlowerView.as_view()),
-    path('', static_main, name='main'),
     path('users/', UserView.as_view()),
     path('orders/', OrderView.as_view()),
     path(r'index/', IndexView.as_view(), name='index'),
-    path(r'flower/<int:id>', FlowerView.as_view(), name='department')
+    path(r'flower/<int:id>', FlowerView.as_view(), name='department'),
+
+    path('', static_main, name='main'),
 ]
