71c6c0bac79a53134416c12a13c390145fbef3bc
Rumyantsev Oleg <rumyantsevok@yandex-team.ru>
2019-01-10 02:54:25 +0300

    Registration working

--- main_app/forms/UserForms.py
+++ main_app/forms/UserForms.py
@@ -15,9 +15,20 @@ class CustomUserRegistrationForm(RegistrationForm):
             'email',
             'password1',
             'password2',
+            'avatar'
         )
 
+    def save(self, commit=True):
+        user = super(CustomUserRegistrationForm, self).save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.email = self.cleaned_data['email']
+        user.avatar = self.cleaned_data['avatar']
 
+        if commit:
+            user.save()
+
+        return user
 
 
 class CustomUserChangeForm(UserChangeForm):
